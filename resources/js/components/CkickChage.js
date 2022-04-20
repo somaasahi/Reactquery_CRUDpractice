3f8d8d1b0fd2e6d6b3bd17c86945b82ac6b52d30
soma_asahi <95704277+somaasahi@users.noreply.github.com>
2022-04-20 23:31:21 +0900

    before try tailwind setup

---
+++ resources/js/components/CkickChage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { useQueryClient } from 'react-query';
+
+function ClickChange() {
+
+
+    return (
+        <div>
+           clickchage
+        </div>
+    );
+}
+
+export default ClickChange;
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<ClickChange />, document.getElementById('app'));
+}
